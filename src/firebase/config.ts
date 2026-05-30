1671aee4c15e6be2b359ad06800172de795f48a9
gvconecta2025 <gvconecta2025@gmail.com>
2026-05-29 21:25:19 -0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,8 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
+// A palavra 'export' aqui resolve o erro TS2459 exigido pela Vercel
+export const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
